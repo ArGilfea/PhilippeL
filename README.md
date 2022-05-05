aba2c678f4c8ed481ca5ae21e7a4d12477e421a2
Philippe L <philippe.laporte.3@umontreal.ca>
2022-05-05 12:57:54 -0400

    I tried
    I tried to make the changes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Repository for git training
 
 Here we can write in markup! This is the perfect place to advertise/explain your code :)
+
+Hey, I tried stuff!
--- test.py
+++ test.py
@@ -8,8 +8,8 @@ After a successful run, please submit a pull request!
 def read_input():
     # Please allow the user to enter their first and last name from the terminal
     # Hint: We call this a raw input
-    first_name = ;
-    last_name = ;
+    first_name = 'Philippe';
+    last_name = 'Laporte';
 
     # Please fix the following print statement
     'Welcome %s %s'%(first_name,last_name)
